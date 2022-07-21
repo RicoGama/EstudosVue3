90579a113e01cad49b1d32eb06208d58296acaa5
Ricardo Gama <ricard.gama@gmail.com>
2022-07-21 18:03:00 -0300

    Fix problems eslint

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeComponent from '../views/Home.vue'
+import BasketComponent from '../views/Basket.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: HomeComponent
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/basket',
+    name: 'Basket',
+    component: BasketComponent
+  },
+  
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
